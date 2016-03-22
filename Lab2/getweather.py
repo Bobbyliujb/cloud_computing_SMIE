73052acd32bfa35cadd64363db470d3c0278f6e4
Bobby <1181822834@qq.com>
2016-03-22 06:34:25 -0700

    Finish parsing json from weather.com

--- Lab2/getweather.py
+++ Lab2/getweather.py
@@ -3,6 +3,7 @@ import urllib2
 import json
 import city  #这个文件在本工程里面已经给出，需要下载到本地
 
+
 url = 'http://www.weather.com.cn/data/cityinfo/%s.html'
 #url = 'http://m.weather.com.cn/data/%s.html'
 #天气网API使用的是城市编码，城市编码在city.py文件中已经给出，上面的url的%s部分就是需要填充的城市编码部分
@@ -17,10 +18,8 @@ code = city.citycode[city_name]  #通过字典的key获得value
 
 res = urllib2.urlopen(url%code)
 a = res.read()
-print a
 data = json.loads(a)
-print data
-# TODO:......
+print u"%s,%s,%s 到%s ." % (data["weatherinfo"]["city"], data["weatherinfo"]["weather"], data["weatherinfo"]["temp2"], data["weatherinfo"]["temp1"])  # 在前面加u强制以unicode输出中文而不是ascii
 
 #有兴趣的同学可以使用下面这个url然后进行解析
 #url = 'http://m.weather.com.cn/data/%s.html'
